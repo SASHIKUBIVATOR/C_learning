96934374c337c3456d4a83dfcaddf70317e21c3a
sashik <mrnormalnybog@gmail.com>
2023-10-26 20:33:14 +1000

    calculator

--- lesson5_calc.cpp
+++ lesson5_calc.cpp
@@ -1,3 +1,35 @@
-//
-// Created by sashik on 26/10/23.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2;
+    float res;
+    bool flag = false;
+    cout << flag;
+    cout << "Enten num1\n";
+    cin >> num1;
+
+    cout << "Enten num2\n";
+    cin >> num2;
+
+    char math;
+    cout << "Enten math symbol\n";
+    cin >> math;
+        switch (math) {
+            case '+':
+                res = num1 + num2;
+                break;
+            case '-':
+                res = num1 + num2;
+                break;
+            case '*':
+                res = num1 + num2;
+                break;
+            case '/':
+                res = num1 + num2;
+                break;
+            default:
+                cout << "Error, try again" << endl;
+        }
+    cout << "Result: " << res << endl;
+}
